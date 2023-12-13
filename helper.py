95d1e1065c56926c72b5a4711c0047ab4efb5f17
Tim Arni <tim.arni@epfl.ch>
2023-12-13 20:18:14 +0100

    clean code

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def seperate_path(path):
     # Split the path into individual articles
     articles = path.split(";")
@@ -18,19 +20,19 @@ def find_article_index(article, helper_df):
         return None
 
 # Calculate the shortest distance between source and target indices
-def calculate_shortest_distance(source_idx, target_idx):
+def calculate_shortest_distance(source_idx, target_idx, table_distance):
     try:
         return table_distance[source_idx, target_idx][0]
     except KeyError:
         return None
 
 # Finding the shortest path length between a source and a target
-def find_path_distance(source, target, helper_df):
+def find_path_distance(source, target, helper_df, table_distance):
     source_idx = find_article_index(source, helper_df)
     target_idx = find_article_index(target, helper_df)
 
     if source_idx is not None and target_idx is not None:
-        return calculate_shortest_distance(source_idx, target_idx)
+        return calculate_shortest_distance(source_idx, target_idx, table_distance)
     else:
         # Handle the case where source or target is not present in the index
         return None
@@ -45,3 +47,22 @@ def extract_subject_category(category):
     # Check if there is at least one word after 'subject'
     if subject_index < len(category_parts) - 1:
         return category_parts[subject_index + 1] 
+    
+
+def get_region(country, DataFrame_with_regions, names_to_replace):
+    """
+    Returns the region for country, using names_to_replace for those with different names in DataFram_with_region than in the Wikispeedia Dataset
+    """
+    country = country.replace('_',' ')
+    if country in DataFrame_with_regions['Country or Area'].values:
+        region = DataFrame_with_regions[DataFrame_with_regions['Country or Area'] == country]['Sub-region Name'].values
+        return region[0]
+    elif names_to_replace.get(country) != None:
+        new_country = names_to_replace[country]
+        region = DataFrame_with_regions[DataFrame_with_regions['Country or Area'] == new_country]['Sub-region Name'].values
+        if len(region) > 0:
+            return region[0]
+        else:
+            return ""
+    else:
+        return np.nan
